8345a5462c8c9401f79154f471b2935ef29bfce9
Tuan Pham <TuanPham@Tuans-MacBook-Pro.local>
2021-05-26 16:11:48 +0100

    update

--- postcode_task.py
+++ postcode_task.py
@@ -5,11 +5,15 @@
 # display the data in user friendly format
 
 import requests
-postcode_api = "https://api.postcodes.io/postcodes/"
-postcode = input("Please enter your postcode: ")
-user_postcode = postcode_api + postcode
-post_api_response = requests.get(user_postcode)
-if post_api_response == 200:
-    print("That is a valid postcode, thank you! ")
-else:
-    print("Sorry the postcode you entered is incorrect, please enter the correct postcode! ")
+
+def postcode_check():
+    postcode_api = "https://api.postcodes.io/postcodes/"
+    postcode = input("Please enter your postcode, without any spaces please: ""\n").lower()
+    user_postcode = (f"https://api.postcodes.io/postcodes/{postcode}")
+    post_api_response = requests.get(user_postcode)
+    if post_api_response.status_code == 200:
+        print("That is a valid postcode, thank you! ")
+    else:
+        print("Sorry the postcode you entered is incorrect, please enter the correct postcode! ")
+
+postcode_check()
